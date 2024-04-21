0b658be7c8c10a633be08112914d2e798c37f6de
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-04-21 20:08:48 +0530

    Update index.js
    front end connected

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const path = require("path");
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://portfolio-alpha-seven-86.vercel.app/",
     methods: "GET, POST, PUT, DELETE, HEAD",
     credentials: true,
 };
